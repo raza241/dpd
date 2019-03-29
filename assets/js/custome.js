9fb41402f37ceef0366abd844f9259234038cc5e
raza241 <raza241@yahoo.com>
2019-03-29 15:18:57 +0500

    intial commit

---
+++ assets/js/custome.js
@@ -0,0 +1,124 @@
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// incerment + Decrement
+function incrementValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+
+function decrementValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+}
+
+// ===========SideBar || Cart
+jQuery(document).ready(function ($) {
+    //if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
+    var $L = 200,
+        $menu_navigation = $('#main-nav'),
+        $cart_trigger = $('#cd-cart-trigger'),
+        $hamburger_icon = $('#cd-hamburger-menu'),
+        $lateral_cart = $('#cd-cart'),
+        $shadow_layer = $('#cd-shadow-layer');
+
+    //open lateral menu on mobile
+    $hamburger_icon.on('click', function (event) {
+        event.preventDefault();
+        //close cart panel (if it's open)
+        $lateral_cart.removeClass('speed-in');
+        toggle_panel_visibility($menu_navigation, $shadow_layer, $('body'));
+    });
+
+    //open cart
+    $cart_trigger.on('click', function (event) {
+        event.preventDefault();
+        //close lateral menu (if it's open)
+        $menu_navigation.removeClass('speed-in');
+        toggle_panel_visibility($lateral_cart, $shadow_layer, $('body'));
+    });
+
+    //close lateral cart or lateral menu
+    $shadow_layer.on('click', function () {
+        $shadow_layer.removeClass('is-visible');
+        // firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+        if ($lateral_cart.hasClass('speed-in')) {
+            $lateral_cart.removeClass('speed-in').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                $('body').removeClass('overflow-hidden');
+            });
+            $menu_navigation.removeClass('speed-in');
+        } else {
+            $menu_navigation.removeClass('speed-in').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                $('body').removeClass('overflow-hidden');
+            });
+            $lateral_cart.removeClass('speed-in');
+        }
+    });
+
+    //move #main-navigation inside header on laptop
+    //insert #main-navigation after header on mobile
+    move_navigation($menu_navigation, $L);
+    $(window).on('resize', function () {
+        move_navigation($menu_navigation, $L);
+
+        if ($(window).width() >= $L && $menu_navigation.hasClass('speed-in')) {
+            $menu_navigation.removeClass('speed-in');
+            $shadow_layer.removeClass('is-visible');
+            $('body').removeClass('overflow-hidden');
+        }
+
+    });
+});
+
+function toggle_panel_visibility($lateral_panel, $background_layer, $body) {
+    if ($lateral_panel.hasClass('speed-in')) {
+        // firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+        $lateral_panel.removeClass('speed-in').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+            $body.removeClass('overflow-hidden');
+        });
+        $background_layer.removeClass('is-visible');
+
+    } else {
+        $lateral_panel.addClass('speed-in').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+            $body.addClass('overflow-hidden');
+        });
+        $background_layer.addClass('is-visible');
+    }
+}
+
+// =================== Custome DropDown
+/* JS for demo only */
+var colors = ['1abc9c', '2c3e50', '2980b9', '7f8c8d', 'f1c40f', 'd35400', '27ae60'];
+
+colors.each(function (color) {
+  $$('.color-picker')[0].insert(
+    '<div class="square" style="background: #' + color + '"></div>'
+  );
+});
+
+$$('.color-picker')[0].on('click', '.square', function(event, square) {
+  background = square.getStyle('background');
+  $$('.custom-dropdown select').each(function (dropdown) {
+    dropdown.setStyle({'background' : background});
+  });
+});
+
+/*
+ * Original version at
+ * http://red-team-design.com/making-html-dropdowns-not-suck
+ */
